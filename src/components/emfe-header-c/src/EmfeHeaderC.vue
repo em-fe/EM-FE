37f803d54f1b76557e5f5a7e0d9b01b5c6b731fa
ZhouWeiru <604286433@qq.com>
2017-09-28 14:58:45 +0800

    fix: headerc page

--- src/components/emfe-header-c/src/EmfeHeaderC.vue
+++ src/components/emfe-header-c/src/EmfeHeaderC.vue
@@ -2,16 +2,19 @@
   <header class="emfe-header-c">
     <div class="emfe-header-c-main">
       <div class="emfe-header-c-eye">
-        <img :src="logo" height="100%">
+        <emfe-icon type="logo" className="emfe-header-c-eye"></emfe-icon>
       </div>
       <div class="emfe-header-c-info">
-        <span class="emfe-header-c-account">活动易首页 你好，{{ user }}</span>
+        <span class="emfe-header-c-account" v-if="!loginFlg">活动易首页 你好，
+          <emfe-link class="emfe-header-c-logout" :routers="{}" @click="login">请登录</emfe-link>  
+        </span>
+        <span class="emfe-header-c-account" v-if="loginFlg">活动易首页 你好，{{ user }}</span>
         <emfe-link className="emfe-header-c" :routers="orderRouter" @click="orderClick">我的订单</emfe-link>
         <span>
-          <emfe-icon className="emfe-header-c" type="hint"></emfe-icon>
-          <emfe-link className="emfe-header-c" :routers="followRouter" @click="followClick">购物车 ({{ followNum }})</emfe-link>
+          <emfe-icon className="emfe-header-c" type="gouwuche"></emfe-icon>
+          <emfe-link className="emfe-header-c" :routers="followRouter" @click="followClick">购物车</emfe-link>
         </span>
-        <emfe-link class="emfe-header-c-logout" :routers="{}" @click="logout">退出</emfe-link>
+        <emfe-link class="emfe-header-c-logout" :routers="{}" @click="logout" v-if="loginFlg">退出</emfe-link>
       </div>
     </div>
   </header>
@@ -20,13 +23,8 @@
 export default {
   name: 'EmfeHeaderC',
   props: {
-    logo: {
-      type: String,
-      required: true,
-    },
     user: {
       type: String,
-      required: true,
     },
     orderRouter: {
       type: [Object, String],
@@ -36,9 +34,16 @@ export default {
       type: [Object, String],
       required: true,
     },
-    followNum: {
-      type: Number,
+    loginFlg: {
+      type: Boolean,
       required: true,
+    },
+  },
+  watch: {
+    loginFlg(val, oldVal) {
+      if (val !== oldVal) {
+        this.loginFlg = val;
+      }
     },
   },
   methods: {
@@ -50,6 +55,10 @@ export default {
     },
     followClick() {
       this.$emit('followClick');
+    },
+    login() {
+      this.$router.push({ name: 'PcLogin' });
+      this.$emit('login');
     },
   },
 };
