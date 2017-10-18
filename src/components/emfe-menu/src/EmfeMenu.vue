7e04a43019abb1e65d78a2469c078d29f772de10
wangzhenjie <wangzhenjie@sporte.cn>
2017-10-18 20:02:56 +0800

    fix table and menu

--- src/components/emfe-menu/src/EmfeMenu.vue
+++ src/components/emfe-menu/src/EmfeMenu.vue
@@ -106,9 +106,12 @@ export default {
       }
     };
 
-    resizeHandle();
-
-    window.addEventListener('resize', resizeHandle);
+    this.datas.forEach((item) => {
+      if (O.hOwnProperty(item, 'children')) {
+        resizeHandle();
+        window.addEventListener('resize', resizeHandle);
+      }
+    });
   },
   methods: {
     handle(val) {
