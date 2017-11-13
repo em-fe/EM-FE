9274c87ac56b767e507cde3978fcc8f16e17f69d
李梦龙 <649076408@qq.com>
2017-11-13 11:27:34 +0800

    feature: add changecount classname and time

--- examples/esm/components/functions/ChangecountPage.vue
+++ examples/esm/components/functions/ChangecountPage.vue
@@ -1,8 +1,50 @@
 <template>
   <div class="changecount">
-    <emfe-changecount :count="count"></emfe-changecount>
-    <button @click="add" type="button" name="button">点击</button>
-    <button @click="add1" type="button" name="button">点击</button>
+    <h3>基本用法</h3>
+    <br>
+    当前累计：<emfe-changecount :count="count" style="display: inline-block"></emfe-changecount>次
+    <br>
+    <br>
+    <emfe-link :routers="{}" @click="add">增加点击</emfe-link>
+    <emfe-link :routers="{}" @click="reduce">减少点击</emfe-link>
+    <br>
+    <br>
+    <pre>
+&lt;emfe-changecount :count="count"&gt;&lt;/emfe-changecount&gt;
+    </pre>
+    <br>
+    <br>
+    <h3>API</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>属性</th>
+          <th>说明</th>
+          <th>类型</th>
+          <th>默认值</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>count</td>
+          <td>内容</td>
+          <td>Number</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>time</td>
+          <td>运动时间</td>
+          <td>Number</td>
+          <td>0.3</td>
+        </tr>
+        <tr>
+          <td>className(class-name)</td>
+          <td>自定义的 class 名称。</td>
+          <td>String</td>
+          <td>-</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
@@ -10,15 +52,15 @@ export default {
   name: 'changecountPage',
   data() {
     return {
-      count: 292,
+      count: Math.random() * 10000 >> 0,
     };
   },
   methods: {
     add() {
-      this.count += 6;
+      this.count = Math.random() * 100 >> 0;
     },
-    add1() {
-      --this.count;
+    reduce() {
+      this.count = Math.random() * 1000 >> 0;
     },
   },
 };
--- src/components/emfe-changecount/src/EmfeChangecount.vue
+++ src/components/emfe-changecount/src/EmfeChangecount.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="emfe-changecount">
-    <div v-for="(l,i) in countList" :style="{top: -currentIndex[i]+ 'px',left: i*15 + 'px'}" class="emfe-changecount-box">
-      <span class="emfe-changecount-box-count" v-for="(item, index) in countArr">{{item}}</span>
+  <div class="emfe-changecount" :class="changecountName">
+    <div v-for="(l,i) in countList" :style="{transform: `translate3d(0, ${-currentIndex[i]}px, 0)`, 'transition-duration': `${time}s`}" class="emfe-changecount-box" :class="boxName">
+      <span class="emfe-changecount-box-count" v-for="(item, index) in countArr" :class="countName">{{item}}</span>
     </div>
   </div>
 </template>
 
 <script>
+const itemHeight = 28; // 数字的高度
+
 export default {
   name: 'EmfeChangecount',
   props: {
@@ -14,47 +16,56 @@ export default {
       type: Number,
       default: 0,
     },
+    time: {
+      type: Number,
+      default: 0.3,
+    },
+    className: String,
+  },
+  computed: {
+    changecountName() {
+      return this.className ? `${this.className}-changecount` : '';
+    },
+    boxName() {
+      return this.className ? `${this.className}-changecount-box` : '';
+    },
+    countName() {
+      return this.className ? `${this.className}-changecount-box-count` : '';
+    },
   },
   data() {
     return {
       countArr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      copycountArr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       countList: [],
       currentIndex: [],
-      index: [0],
     };
   },
   created() {
-    this.countList = this.count.toString().split('');
-    for (let k = 0; k < this.countList.length; k++) {
-      this.currentIndex.push(0);
-    }
-    setTimeout(() => {
-      this.move();
-    }, 50);
+    this.init();
+    this.move();
   },
   methods: {
-    move() {
-      this.time = [];
-      this.currentIndex = [];
+    // 初始化
+    init() {
       this.countList = this.count.toString().split('');
-      for (let k = 0; k < this.countList.length; k++) {
-        for (let i = 0; i < this.countArr.length; i++) {
-          if (this.countArr[i] === Number(this.countList[k])) {
-            this.currentIndex.push(i * 28);
-            break;
-          }
+      this.countList.forEach(() => this.currentIndex.push(0));
+    },
+    // 动起来
+    move() {
+      setTimeout(() => {
+        this.currentIndex = [];
+
+        for (let k = 0; k < this.countList.length; k++) {
+          this.currentIndex.push(this.countList[k] * itemHeight);
         }
-      }
+      }, 0);
     },
   },
   watch: {
     count(val, oldval) {
       if (val !== oldval) {
         this.count = val;
-        this.copycountArr.forEach((item) => {
-          this.countArr.push(item);
-        });
+        this.init();
         this.move();
       }
     },
