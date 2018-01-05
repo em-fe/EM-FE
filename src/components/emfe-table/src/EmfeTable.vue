764a43b220b8ec9227874ddf966b75c08df37d7b
zhouweiru <604286433@qq.com>
2018-01-05 15:27:52 +0800

    fix: table fixed right and left

--- src/components/emfe-table/src/EmfeTable.vue
+++ src/components/emfe-table/src/EmfeTable.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="emfe-table-box" :class="[boxName]">
-    <table v-if="columns.length" class="emfe-table" :class="[tableName]">
-      <slot name="head"></slot>
-      <tbody :class="tbodyName">
-        <tr v-if="!data.length">
-          <td class="emfe-table-tr-td emfe-table-nothing-td" :colspan="columns.length">
-            <div class="emfe-table-nothing">{{nothingText}}</div>
-          </td>
-        </tr>
-        <slot name="body" else></slot>
-      </tbody>
-    </table>
+  <div class="emfe-table-box" :class="[boxName, {'emfe-table-box-flex': flex}]">
+    <div :class="[flexTableName, {'emfe-table-tablebox': flex}]">
+      <table v-if="columns.length" class="emfe-table" :class="[tableName]">
+        <slot name="head"></slot>
+        <tbody :class="tbodyName">
+          <tr v-if="!data.length">
+            <td class="emfe-table-tr-td emfe-table-nothing-td" :colspan="columns.length">
+              <div class="emfe-table-nothing">{{nothingText}}</div>
+            </td>
+          </tr>
+          <slot name="body" else></slot>
+        </tbody>
+      </table>
+    </div>
+    <div class="emfe-table-fixbox" v-if="flex" :class="flexName">
+      <div class="emfe-table-fixbox-item" v-for="(flexItem, flexIndex) in flexColumns">
+        <div class="emfe-table-fixbox-item-title">
+          <div class="emfe-table-fixbox-item-title-name" :class="flexItemName">
+            {{flexItem.title}}
+          </div>
+        </div>
+        <div class="emfe-table-fixbox-item-main">
+          <div class="emfe-table-fixbox-item-main-name" v-for="(opationItem, opationIndex) in flexItem.opation" :class="flexItemName">
+            {{opationItem.name}}
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- <div class="emfe-table-nothing" v-if="!data.length">{{nothingText}}</div> -->
   </div>
 </template>
@@ -52,6 +68,19 @@ export default {
       default: '',
     },
     className: String,
+    flex: String,
+    flexColumns: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    flexData: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
   },
   computed: {
     boxName() {
@@ -62,6 +91,15 @@ export default {
     },
     tbodyName() {
       return this.classAddName || this.className ? `${this.classAddName || this.className}-table-tbody` : '';
+    },
+    flexName() {
+      return this.flex ? `emfe-table-fixbox-${this.flex}` : '';
+    },
+    flexItemName() {
+      return this.flex ? `emfe-table-fixbox-${this.flex}-item-name` : '';
+    },
+    flexTableName() {
+      return this.flex ? `emfe-table-tablebox-${this.flex}` : '';
     },
   },
 };
